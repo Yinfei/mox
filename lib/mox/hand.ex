2e55796add41508edfb7d35fb1be9396c0f0565d
Yinfei <yinfei84@gmail.com>
2018-04-08 16:05:20 +0200

    Add mulligan function

--- lib/mox/hand.ex
+++ lib/mox/hand.ex
@@ -2,4 +2,10 @@ defmodule Mox.Hand do
   def new(deck) do
     Enum.take_random(deck, 7)
   end
+
+  def mulligan(_, []), do: []
+
+  def mulligan(deck, hand) do
+    Enum.take_random(deck, Enum.count(hand) - 1)
+  end
 end
